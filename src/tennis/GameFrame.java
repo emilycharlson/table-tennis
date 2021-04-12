79ea9f4f2325e4ec5758256af79eab05f009d936
ECharlon <emily@emilycharlson.com>
2021-04-12 18:53:52 -0400

    added methods

--- src/tennis/GameFrame.java
+++ src/tennis/GameFrame.java
@@ -4,6 +4,22 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class GameFrame {
-
+// JFrame is a top-level container that provides a window on the screen 
+public class GameFrame extends JFrame {
+	
+	GamePanel panel;
+	
+	
+	GameFrame() {
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//will adjust accordingly to accommodate the size of the game panel.
+		this.pack();
+		this.setVisible(true);
+	}
+	
 }
